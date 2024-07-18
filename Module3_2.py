85aebbab32512986b5a1f79a7a2935c3e55777bd
Maximovex <maximove81@gmail.com>
2024-07-18 09:29:13 +0300

    Create Module3_2.py

---
+++ Module3_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    email_ext=('.com','.ru','.net','.org')
+    allright=False
+    if '@'in (recipient and sender):
+        for i in email_ext:
+            if i in (recipient and sender):
+                allright= True
+                break
+        else:
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        if allright: 
+            if recipient==sender:
+                print('Нельзя отправить письмо самому себе!')
+            if sender=='university.help@gmail.com':
+                print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+            else:
+                print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+
+send_email('Это сообщение для проверки связи', 'vasyok@1337gmail')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fanmail.ru', sender='urban.infogmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
