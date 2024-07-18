0e90686592527c92cd10957040f2347976923a06
Maximovex <maximove81@gmail.com>
2024-07-18 20:47:13 +0300

    Update Module3_1.py

--- Module3_1.py
+++ Module3_1.py
@@ -10,13 +10,14 @@ def string_info(string):
 def is_contains(string, list_to_search):
     count_calls()
     string=string.lower()
-    list_to_search=list_to_search.lower()
+    for i in range(len(list_to_search)):
+        list_to_search[i]=list_to_search[i].lower()
     if string in list_to_search:
         is_contains=True
     else:
         is_contains=False
     return is_contains
 print(string_info('Rooster'))
-print(is_contains('apple','ApplePie'))
+print(is_contains('apple',['ApplePie','appleton']))
 print(string_info('Rollerline'))
 print(calls)
